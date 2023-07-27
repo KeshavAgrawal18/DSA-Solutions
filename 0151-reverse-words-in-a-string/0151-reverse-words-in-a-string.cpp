57514b0aa42914b9a57ed5386adcb6f90a4021b7
KeshavAgrawal18 <98193032+KeshavAgrawal18@users.noreply.github.com>
2023-07-27 07:04:32 +0530

    Time: 4 ms (80.23%), Space: 7.2 MB (86.78%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,26 +1,35 @@
-class Solution {
+class Solution
+{
 public:
-    string reverseWords(string s) {
-        string ans;
-        string temp;    
-        for(int i=s.size()-1; i>=0; i--){
-            
-        while(s[i] == ' ') {
-            i--;
-            if(i<0) return ans;
-        }
-            if(i < 0) return ans;
-            while(s[i] != ' ') {
+    string reverseWords(string s)
+    {
+        string ans, temp;
+        for (int i = s.size() - 1; i >= 0; i--)
+        {
+
+            while (s[i] == ' ')
+            {
+                i--;
+                if (i < 0)
+                    return ans;
+            }
+            if (i < 0)
+                return ans;
+            while (s[i] != ' ')
+            {
                 temp.push_back(s[i]);
                 i--;
-                if(i<0) break;
+                if (i < 0)
+                    break;
             }
-            if(ans.size()!=0) ans.push_back(' ');
-            for(int j=temp.size()-1; j>=0; j--){
+            if (ans.size() != 0)
+                ans.push_back(' ');
+            for (int j = temp.size() - 1; j >= 0; j--)
+            {
                 ans.push_back(temp[j]);
             }
-           temp.clear();
+            temp.clear();
         }
-         return ans;
+        return ans;
     }
 };
