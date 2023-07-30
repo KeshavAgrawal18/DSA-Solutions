6bbe0d6936f4874041645695aa74f932db493e36
KeshavAgrawal18 <98193032+KeshavAgrawal18@users.noreply.github.com>
2023-07-30 06:56:40 +0530

    Time: 46 ms (83.11%), Space: 24.4 MB (47.09%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    int lcsRecursion(vector<vector<int>>& dp, string& a, string& b, int n1, int n2){
-        // Top Down Approach
-        // Remember If the length of String is more Don't make it's copy again and again in recursion
-        // Instead Use reference to original string 
-        //It optimizes the code to large extent upto 2.92s to 0.04s
-        
-        if(n1<0 || n2<0) return 0;
-        if(dp[n1][n2] != -1) return dp[n1][n2];
-        
-        if(a[n1] == b[n2]) {
-            return dp[n1][n2] = 1 + lcsRecursion(dp, a, b, n1-1, n2-1);
-        }
-        else return dp[n1][n2] = max(lcsRecursion(dp, a, b, n1, n2-1), lcsRecursion(dp, a, b, n1-1, n2));
-   
-    }
     
     int longestCommonSubsequence(string text1, string text2) {
     
         int n1 = text1.size(), n2 = text2.size();
-        vector<vector<int>> dp(n1, vector<int>(n2, -1));
-        
-        return lcsRecursion(dp, text1, text2, n1-1, n2-1);
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
+        for(int i=1; i<=n1; i++){
+            for(int j=1; j<=n2; j++){
+                {
+                    if(text1[i-1] == text2[j-1]){
+                        dp[i][j] = 1 + dp[i-1][j-1];
+                    }
+                    else{
+                         dp[i][j] = max( dp[i-1][j], dp[i][j-1] );
+                    }
+                }
+            }
+        }
+        return dp[n1][n2];
     }
 };
