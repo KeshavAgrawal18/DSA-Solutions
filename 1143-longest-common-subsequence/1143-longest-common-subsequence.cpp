1ab1034880a1bbc721153f10dba726e633e8cd18
KeshavAgrawal18 <98193032+KeshavAgrawal18@users.noreply.github.com>
2023-07-30 06:45:44 +0530

    Time: 58 ms (74.88%), Space: 24.6 MB (25.27%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int lcsRecursion(vector<vector<int>>& dp, string& a, string& b, int n1, int n2){
+        // Top Down Approach
+        // Remember If the length of String is more Don't make it's copy again and again in recursion
+        // Instead Use reference to original string 
+        //It optimizes the code to large extent upto 2.92s to 0.04s
         
         if(n1<0 || n2<0) return 0;
         if(dp[n1][n2] != -1) return dp[n1][n2];
