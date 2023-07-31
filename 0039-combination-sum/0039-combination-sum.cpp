e070796c329313509b5c2ef8dcad3b1f795de933
KeshavAgrawal18 <98193032+KeshavAgrawal18@users.noreply.github.com>
2023-07-31 20:33:29 +0530

    Time: 12 ms (42.83%), Space: 12.1 MB (43.37%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -9,13 +9,14 @@ public:
         if(sum > target) return;
         for(auto i = start; i<candidates.size();i++){
             // if(temp.size()>0 && i < temp[temp.size()-1]) continue;   
-            if(sum + candidates[i] > target) continue;
+            if(sum + candidates[i] > target) break;
             temp.push_back(candidates[i]);
             fun(temp, candidates, target, sum + candidates[i], i);
             temp.pop_back();
         }
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
         fun({}, candidates, target, 0, 0);
         return ans;
     }
