d5d1e800db555447dac787ee214539b126a00151
KeshavAgrawal18 <98193032+KeshavAgrawal18@users.noreply.github.com>
2023-08-01 07:06:25 +0530

    Time: 20 ms (86.34%), Space: 10.1 MB (42.00%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> temp;
+    int k, n;
+    void fun(int start){
+        if(temp.size() == k){
+          ans.push_back(temp);
+            return;
+        } 
+        for(int i = start; i<=n; i++){
+            temp.push_back(i);
+            fun(i+1);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int N, int K) {
+        k = K;
+        n = N;
+        fun(1);
+        return ans;
+    }
+};
